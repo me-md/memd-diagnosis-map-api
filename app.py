b64146951d03f750433787622f9ca30df3e0369c
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-01-07 17:02:33 -0700

    Configure .env

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+print(os.environ['APP_SETTINGS'])
+
+from models import Condition
+
 
 @app.route('/')
 def hello():
-    return "Hurray! We are up and running!!!"
+    return "Hello World!"
 
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ from flask.ext.migrate import Migrate, MigrateCommand
 
 from app import app, db
 
-
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
